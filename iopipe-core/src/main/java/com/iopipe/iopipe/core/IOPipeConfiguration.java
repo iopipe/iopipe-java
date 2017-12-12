216b62645926ffe2114a6a15241944a8771df7d0
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-12-12 14:21:01 -0500

    Add base class for IOPipe configuration class.

---
+++ iopipe-core/src/main/java/com/iopipe/iopipe/core/IOPipeConfiguration.java
@@ -0,0 +1,49 @@
+package com.iopipe.iopipe.core;
+
+/**
+ * This class contains the configuration for IOPipe and specifies the settings
+ * which are to be used when the server is contacted.
+ *
+ * This class is mutable.
+ *
+ * @since 2017/12/12
+ */
+public final class IOPipeConfiguration
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/12/12
+	 */
+	@Override
+	public final boolean equals(Object __o)
+	{
+		if (this == __o)
+			return true;
+		
+		if (!(__o instanceof IOPipeConfiguration))
+			return false;
+		
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/12/12
+	 */
+	@Override
+	public int hashCode()
+	{
+		throw new Error("TODO");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/12/12
+	 */
+	@Override
+	public String toString()
+	{
+		throw new Error("TODO");
+	}
+}
+
