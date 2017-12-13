7f16ba2d81cfcbc366c740456d9490b3d628e08b
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-12-13 14:00:23 -0500

    Remove the old handler wrappers and have classes which handle two cases using two different means (extending and calling).

---
+++ src/main/java/com/iopipe/awslambda/CallingStreamWrapper.java
@@ -0,0 +1,57 @@
+package com.iopipe.awslambda;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.iopipe.IOPipeConfiguration;
+
+/**
+ * This class is used to call other stream handlers.
+ *
+ * To see more information on how this class is to be used, see the package
+ * information.
+ *
+ * This class may be extended by a sub-class or initialized by another class
+ * when it is needed to call an existing handler, it is recommended that it
+ * is extended.
+ *
+ * @since 2017/12/13
+ */
+public class CallingStreamWrapper
+	implements RequestStreamHandler
+{
+	/** The class to be initialized and called. */
+	protected final Class<RequestStreamHandler> call;
+	
+	/**
+	 * Initializes the calling wrapper which sends the request to an instance
+	 * of the specified class.
+	 *
+	 * @param __call The class to call to handle the request.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2017/12/13
+	 */
+	public CallingStreamWrapper(Class<RequestStreamHandler> __call)
+		throws NullPointerException
+	{
+		if (__call == null)
+			throw new NullPointerException();
+		
+		this.call = __call;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/12/13
+	 */
+	@Override
+	public final void handleRequest(InputStream __in, OutputStream __out,
+		Context __c)
+		throws IOException
+	{
+		throw new Error("TODO");
+	}
+}
+
---
+++ src/main/java/com/iopipe/awslambda/CallingWrapper.java
@@ -0,0 +1,55 @@
+package com.iopipe.awslambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.iopipe.IOPipeConfiguration;
+
+/**
+ * This class is used to call other request handlers for input and output
+ * of basic Java types and Plain Old Java Objects.
+ *
+ * To see more information on how this class is to be used, see the package
+ * information.
+ *
+ * This class may be extended by a sub-class or initialized by another class
+ * when it is needed to call an existing handler, it is recommended that it
+ * is extended.
+ *
+ * @param <I> The input object type.
+ * @param <O> The output object type.
+ * @since 2017/12/13
+ */
+public class CallingWrapper<I, O>
+	implements RequestHandler<I, O>
+{
+	/** The class to be initialized and called. */
+	protected final Class<RequestHandler<I, O>> call;
+	
+	/**
+	 * Initializes the calling wrapper which sends the request to an instance
+	 * of the specified class.
+	 *
+	 * @param __call The class to call to handle the request.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2017/12/13
+	 */
+	public CallingWrapper(Class<RequestHandler<I, O>> __call)
+		throws NullPointerException
+	{
+		if (__call == null)
+			throw new NullPointerException();
+		
+		this.call = __call;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/12/13
+	 */
+	@Override
+	public final O handleRequest(I __input, Context __c)
+	{
+		throw new Error("TODO");
+	}
+}
+
