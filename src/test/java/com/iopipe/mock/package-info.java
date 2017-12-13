efbb5c38fd4b399da367093aa9dad811e9b73a33
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-12-13 13:23:55 -0500

    Remove the test sub-packages and just have tests themselves.

--- src/test/java/com/iopipe/mock/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * This package contains a mock "service" of IOPipe which is used for local
- * testing so that there is no actual network utilization.
- *
- * @since 2017/12/12
- */
-
-package com.iopipe.mock;
-
---
+++ src/test/java/com/iopipe/test/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains tests for the IOPipe Java library.
+ *
+ * @since 2017/12/13
+ */
+
+package com.iopipe.test;
+
