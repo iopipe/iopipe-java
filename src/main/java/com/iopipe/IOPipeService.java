474648780e50c9f21402851654511f7f16e74919
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-12-13 21:34:35 -0500

    Corrections to the method calls; If the service is disabled then call the method as it is.

--- src/main/java/com/iopipe/IOPipeService.java
+++ src/main/java/com/iopipe/IOPipeService.java
@@ -59,7 +59,7 @@ public final class IOPipeService
 		if (__cont == null || __func == null)
 			throw new NullPointerException();
 		
-		this.<Object, Object, Object>run(__cont, (__a, __b) ->
+		this.<Object, Object, Object>run(__cont, null, null, (__a, __b) ->
 			{
 				__func.run();
 				return null;
@@ -82,7 +82,8 @@ public final class IOPipeService
 		if (__cont == null || __func == null)
 			throw new NullPointerException();
 		
-		return this.<Object, Object, R>run(__cont, (__a, __b) -> __func.get());
+		return this.<Object, Object, R>run(__cont, null, null,
+			(__y, __z) -> __func.get());
 	}
 	
 	/**
@@ -91,18 +92,20 @@ public final class IOPipeService
 	 * @param <A> The input type.
 	 * @param <R> The return value.
 	 * @param __cont The Amazon AWS context this is running under.
+	 * @param __a The first parameter.
 	 * @param __func The function to execute.
 	 * @return The result of the function call.
 	 * @throws NullPointerException On null arguments.
 	 * @since 2017/12/13
 	 */
-	public final <A, R> R run(Context __cont, Function<A, R> __func)
+	public final <A, R> R run(Context __cont, A __a, Function<A, R> __func)
 		throws NullPointerException
 	{
 		if (__cont == null || __func == null)
 			throw new NullPointerException();
 		
-		return this.<A, Object, R>run(__cont, (__a, __b) -> __func.apply(__a));
+		return this.<A, Object, R>run(__cont, __a, null,
+			(__y, __z) -> __func.apply(__a));
 	}
 	
 	/**
@@ -112,16 +115,23 @@ public final class IOPipeService
 	 * @param <B> The second parameter type.
 	 * @param <R> The return value.
 	 * @param __cont The Amazon AWS context this is running under.
+	 * @param __a The first parameter.
+	 * @param __b The second parameter.
 	 * @param __func The function to execute.
 	 * @return The result of the function call.
 	 * @throws NullPointerException On null arguments.
 	 * @since 2017/12/13
 	 */
-	public final <A, B, R> R run(Context __cont, BiFunction<A, B, R> __func)
+	public final <A, B, R> R run(Context __cont, A __a, B __b,
+		BiFunction<A, B, R> __func)
 		throws NullPointerException
 	{
 		if (__cont == null || __func == null)
 			throw new NullPointerException();
+		
+		IOPipeConfiguration config = this.config;
+		if (!config.isEnabled())
+			return __func.apply(__a, __b);
 		
 		throw new Error("TODO");
 	}
