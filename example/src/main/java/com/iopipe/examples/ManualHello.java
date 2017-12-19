382787c97c986421bf1a46dc09f665e908b5c83f
Stephanie Gawroriski <xer@multiphasicapps.net>
2017-12-18 19:39:04 -0500

    Add ManualHello example; Make the examples compile using Java 8.

---
+++ example/src/main/java/com/iopipe/examples/ManualHello.java
@@ -0,0 +1,34 @@
+package com.iopipe.examples;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.iopipe.IOPipeService;
+
+/**
+ * This class wraps the simple request handler and just prefixes "Hello" to
+ * any given input string. This manually initializes the service,
+ *
+ * @since 2017/12/18
+ */
+public class ManualHello
+	implements RequestHandler<String, String>
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2017/12/13
+	 */
+	@Override
+	public final String handleRequest(String __input, Context __context)
+	{
+		try (IOPipeService sv = new IOPipeService())
+		{
+			return sv.createContext(__context).<String>run(
+				() ->
+				{
+					__context.getLogger().log("Input: " + __input);
+					return "Hello " + __input + "!";
+				});
+		}
+	}
+}
+
