2a2cf5d4e1ff6a1752d5aae12d30d1a3c37f908b
Pam Selle <pamela.selle@gmail.com>
2018-02-02 13:55:50 -0500

    Fix tag/ver error

--- release_agent.sh
+++ release_agent.sh
@@ -1,8 +1,8 @@
 
 curl -v -X POST -H 'Content-Type: application/json' -d "{
     \"build_parameters\": {
-        \"MVN_RELEASE_TAG\": \"$JAVA_TAG\",
-        \"MVN_RELEASE_VER\": \"v$JAVA_TAG\",
+        \"MVN_RELEASE_TAG\": \"v$JAVA_TAG\",
+        \"MVN_RELEASE_VER\": \"$JAVA_TAG\",
         \"MVN_RELEASE_DEV_VER\": \"$JAVA_TAG_NEXT-SNAPSHOT\",
         \"MVN_RELEASE_USER_EMAIL\": \"dev@iopipe.com\",
         \"MVN_RELEASE_USER_NAME\": \"Via CircleCI\"
