f518fbec64753882c345d21f2d8b2b098cd7db4f
Pam Selle <pamela.selle@gmail.com>
2018-02-07 12:10:45 -0500

    Update example func for key/val per serverless.yml input capability

--- example/src/main/java/com/iopipe/examples/Hello.java
+++ example/src/main/java/com/iopipe/examples/Hello.java
@@ -1,5 +1,8 @@
 package com.iopipe.examples;
 
+import java.util.Collections;
+import java.util.Map;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.iopipe.IOpipeExecution;
 import com.iopipe.plugin.trace.TraceMark;
@@ -8,13 +11,13 @@ import com.iopipe.plugin.trace.TraceUtils;
 import com.iopipe.SimpleRequestHandlerWrapper;
 
 /**
- * This class wraps the simple request handler and just prefixes "Hello" to
- * any given input string.
+ * This class wraps the simple request handler and prefixes "Hello" to
+ * an input object containing {name:"Foo"}
  *
  * @since 2017/12/18
  */
 public class Hello
-	extends SimpleRequestHandlerWrapper<String, String>
+	extends SimpleRequestHandlerWrapper<Map<String,String>, String>
 {
 	/**
 	 * {@inheritDoc}
@@ -22,34 +25,36 @@ public class Hello
 	 */
 	@Override
 	protected final String wrappedHandleRequest(IOpipeExecution __exec,
-		String __input)
+		Map<String,String> request)
 	{
+		String name = request.containsKey("name") ? request.get("name") : null;
+
 		// Send a message to the example plugin
 		__exec.<ExampleExecution>plugin(ExampleExecution.class, (__s) ->
 			{
 				__s.message("I shall say hello!");
-				__s.message(__input);
+				__s.message(name);
 			});
-		
+
 		// Custom metrics which could convey important information
 		__exec.customMetric("hello", "world");
-		
+
 		// Measure performance of this method via the trace plugin
 		try (TraceMeasurement m = TraceUtils.measure(__exec, "math"))
 		{
 			// Add a bunch of numbers together
 			TraceMark addstart = TraceUtils.mark(__exec, "addstart");
-			
+
 			long result = 0;
 			for (int i = 1; i < 10000; i++)
 				result += new Long(i);
-			
+
 			// Multiply a bunch of numbers into the result
 			TraceMark mulstart = TraceUtils.mark(__exec, "mulstart");
-			
+
 			for (int i = 1; i < 10000; i++)
 				result *= new Long(i);
-			
+
 			// How long did multiplication take?
 			TraceUtils.measure(__exec, "multime", mulstart,
 				TraceUtils.mark(__exec, "mulend"));
@@ -57,9 +62,8 @@ public class Hello
 			// Store the result of the math
 			__exec.customMetric("result", (long)result);
 		}
-		
+
 		// Say hello to them!
-		return "Hello " + __input + "!";
+		return "Hello " + name + "!";
 	}
 }
-
