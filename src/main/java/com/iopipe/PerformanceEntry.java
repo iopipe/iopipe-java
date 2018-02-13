93eb127e5648b08739898e4ad5e4f67a4b9434a9
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-02-13 13:43:21 -0500

    Refactor tracing to work as how the service expects it to work.  * This breaks compatibility.

--- src/main/java/com/iopipe/PerformanceEntry.java
+++ src/main/java/com/iopipe/PerformanceEntry.java
@@ -3,62 +3,59 @@ package com.iopipe;
 import java.util.Objects;
 
 /**
- * This interface is used as a base for performance entries and is used to
- * allow access to the common attributes for each entry.
- *
- * All performance entries must be {@link Comparable}, the sort order is the
- * start time in monotonic nanoseconds.
+ * This class represents a single performance entry which defines
  *
  * @since 2018/01/19
  */
-public interface PerformanceEntry
-	extends Comparable<PerformanceEntry>
+public final class PerformanceEntry
+	implements Comparable<PerformanceEntry>
 {
-	/**
-	 * Returns the name of the performance entry.
-	 *
-	 * @return The performance entry name.
-	 * @since 2018/01/19
-	 */
-	public abstract String name();
+	/** The name of this entry. */
+	protected final String name;
+	
+	/** The type of entry this is. */
+	protected final String type;
+	
+	/** The start time in nanoseconds. */
+	protected final long startns;
+	
+	/** The start time in milliseconds on the system clock. */
+	protected final long startms;
+	
+	/** The duration of the entry in nanoseconds. */
+	protected final long durationns;
 	
 	/**
-	 * Returns the starting time of this performance entry according to the
-	 * high-precision monotonic clock.
+	 * Initializes the performance entry.
 	 *
-	 * This must have a value compatible with {@link System#nanoTime()}.
-	 *
-	 * @return The start time of the entry using the monotic clock.
-	 * @since 2018/01/19
+	 * @param __name The name of this entry.
+	 * @param __type The type of entry this is.
+	 * @param __startns The start time in nanoseconds.
+	 * @param __startms The start time in milliseconds on the system clock.
+	 * @param __durationns The duration of the entry in nanoseconds.
+	 * @throws NullPointerException On null arguments.
+	 * @since 2018/02/13
 	 */
-	public abstract long startNanoTime();
-	
-	/**
-	 * Returns the starting time of this performance entry according to the
-	 * system clock.
-	 *
-	 * This must have a value compatible with
-	 * {@link System#currentTimeMillis()}.
-	 *
-	 * @return The start time of the entry according to the system clock.
-	 * @since 2018/01/19
-	 */
-	public abstract long startTimeMillis();
-	
-	/**
-	 * Returns the type of performance entry this is.
-	 *
-	 * @return The type of performance entry.
-	 * @since 2018/01/19
-	 */
-	public abstract String type();
+	public PerformanceEntry(String __name, String __type, long __startns,
+		long __startms, long __durationns)
+		throws NullPointerException
+	{
+		if (__name == null || __type == null)
+			throw new NullPointerException();
+		
+		this.name = __name;
+		this.type = __type;
+		this.startns = __startns;
+		this.startms = __startms;
+		this.durationns = Math.max(0, __durationns);
+	}
 	
 	/**
 	 * {@inheritDoc}
 	 * @since 2018/01/19
 	 */
 	@Override
-	public default int compareTo(PerformanceEntry __o)
+	public final int compareTo(PerformanceEntry __o)
 		throws NullPointerException
 	{
 		if (__o == null)
@@ -84,13 +81,24 @@ public interface PerformanceEntry
 	 * @return The duration of the entry using the monotic clock.
 	 * @since 2018/01/19
 	 */
-	public default long durationNanoTime()
+	public final long durationNanoTime()
 	{
-		return Math.abs(this.endNanoTime() - this.startNanoTime());
+		return this.durationns;
 	}
 	
 	/**
-	 * Returns the end time of this performance entry according to the
+	 * Returns the name of the performance entry.
+	 *
+	 * @return The performance entry name.
+	 * @since 2018/01/19
+	 */
+	public final String name()
+	{
+		return this.name;
+	}
+	
+	/**
+	 * Returns the starting time of this performance entry according to the
 	 * high-precision monotonic clock.
 	 *
 	 * This must have a value compatible with {@link System#nanoTime()}.
@@ -98,24 +106,35 @@ public interface PerformanceEntry
 	 * @return The start time of the entry using the monotic clock.
 	 * @since 2018/01/19
 	 */
-	public default long endNanoTime()
+	public final long startNanoTime()
 	{
-		return this.startNanoTime();
+		return this.startns;
 	}
 	
 	/**
-	 * Returns the end time of this performance entry according to the
+	 * Returns the starting time of this performance entry according to the
 	 * system clock.
 	 *
 	 * This must have a value compatible with
 	 * {@link System#currentTimeMillis()}.
 	 *
-	 * @return The end time of the entry according to the system clock.
+	 * @return The start time of the entry according to the system clock.
 	 * @since 2018/01/19
 	 */
-	public default long endTimeMillis()
+	public final long startTimeMillis()
 	{
-		return this.startTimeMillis();
+		return this.startms;
+	}
+	
+	/**
+	 * Returns the type of performance entry this is.
+	 *
+	 * @return The type of performance entry.
+	 * @since 2018/01/19
+	 */
+	public final String type()
+	{
+		return this.type;
 	}
 }
 
--- src/main/java/com/iopipe/plugin/trace/TraceUtils.java
+++ src/main/java/com/iopipe/plugin/trace/TraceUtils.java
@@ -21,29 +21,6 @@ public final class TraceUtils
 	}
 	
 	/**
-	 * Creates a new mark which represents a single point in time and adds it
-	 * to the report if the plugin is enabled.
-	 *
-	 * @param __exec The single execution state.
-	 * @param __name The name of the mark to create.
-	 * @return The created mark or {@code null} if the plugin is not enabled.
-	 * @throws NullPointerException On null arguments.
-	 * @since 2018/01/30
-	 */
-	public static TraceMark mark(IOpipeExecution __exec, String __name)
-		throws NullPointerException
-	{
-		if (__exec == null || __name == null)
-			throw new NullPointerException();
-		
-		TraceExecution x = __exec.<TraceExecution>optionalPlugin(
-			TraceExecution.class);
-		if (x != null)
-			return x.mark(__name);
-		return null;
-	}
-	
-	/**
 	 * Creates a new instance of a class which is used to measure how long
 	 * a block of code has executed for. The returned object is
 	 * {@link AutoCloseable} and it is highly recommended to use
@@ -66,40 +43,8 @@ public final class TraceUtils
 		if (__exec == null || __name == null)
 			throw new NullPointerException();
 		
-		TraceExecution x = __exec.<TraceExecution>optionalPlugin(
-			TraceExecution.class);
-		if (x != null)
-			return x.measure(__name);
-		return null;
-	}
-	
-	/**
-	 * Creates a measurement between the two marks, the measurement is
-	 * returned and added to the report if the plugin is enabled..
-	 *
-	 * @param __exec The single execution state.
-	 * @param __name The name of the measurement.
-	 * @param __a The first mark.
-	 * @param __b The second mark.
-	 * @return A performance entry which defines a measurement between
-	 * the two marks, {@code null} is returned if the plugin is not enabled.
-	 * @throws NullPointerException On null arguments.
-	 * @since 2018/01/30
-	 */
-	public static TraceMeasurementMark measure(IOpipeExecution __exec,
-		String __name, TraceMark __a, TraceMark __b)
-		throws NullPointerException
-	{
-		// Do not check __a and __b for null because if a previous execution
-		// returned null then no mark would have been returned.
-		if (__exec == null || __name == null)
-			throw new NullPointerException();
-		
-		TraceExecution x = __exec.<TraceExecution>optionalPlugin(
-			TraceExecution.class);
-		if (x != null)
-			return x.measure(__name, __a, __b);
-		return null;
+		return new TraceMeasurement(__exec.<TraceExecution>optionalPlugin(
+			TraceExecution.class) != null, __exec.measurement(), __name);
 	}
 }
 
