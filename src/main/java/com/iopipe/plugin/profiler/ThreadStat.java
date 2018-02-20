0e9528bcb71483eaf9aff8895c2dfaf26d0aee00
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-02-19 19:18:05 -0500

    Add means for keeping track of nodes within the execution tree.

--- src/main/java/com/iopipe/plugin/profiler/ThreadStat.java
+++ src/main/java/com/iopipe/plugin/profiler/ThreadStat.java
@@ -1,5 +1,8 @@
 package com.iopipe.plugin.profiler;
 
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -8,6 +11,7 @@ import java.util.Objects;
  * @since 2018/02/19
  */
 public final class ThreadStat
+	implements ThreadStatNodeTraversal
 {
 	/** The thread to monitor. */
 	protected final Thread thread;
@@ -20,6 +24,10 @@ public final class ThreadStat
 	
 	/** The name of the thread. */
 	protected final String name;
+	
+	/** Root nodes for the thread tree. */
+	private final Map<MethodTracker.TrackedMethod, Node> _nodes =
+		new LinkedHashMap<>();
 	
 	/** Time spent running the thread in whole time. */
 	private volatile long _wgtime;
@@ -66,6 +74,116 @@ public final class ThreadStat
 		
 		// Add to whole graph time
 		this._wgtime += __rel;
+		
+		// Generate stack trace, since each sub-node based on the origin point
+		// of call is unique per stack trace, methods which call other methods
+		// even though they may result in the same method will have different
+		// timers and such
+		StackTraceElement[] trace = thread.getStackTrace();
+	}
+	
+	/** 
+	 * {@inheritDoc}
+	 * @since 2018/02/19
+	 */
+	@Override
+	public final ThreadStat.Node subNode(MethodTracker.TrackedMethod __m)
+		throws NullPointerException
+	{
+		if (__m == null)
+			throw new NullPointerException();
+		
+		Map<MethodTracker.TrackedMethod, Node> nodes = this._nodes;
+		synchronized (nodes)
+		{
+			Node rv = nodes.get(__m);
+			if (rv == null)
+				nodes.put(__m, (rv = new Node(__m)));
+			return rv;
+		}
+	}
+	
+	/** 
+	 * {@inheritDoc}
+	 * @since 2018/02/19
+	 */
+	@Override
+	public final ThreadStat.Node[] subNodes()
+	{
+		Map<MethodTracker.TrackedMethod, Node> nodes = this._nodes;
+		synchronized (nodes)
+		{
+			Collection<Node> values = nodes.values();
+			return values.<Node>toArray(new Node[values.size()]);
+		}
+	}
+	
+	/**
+	 * This represents a single node within the trace tree.
+	 *
+	 * @since 2018/02/19
+	 */
+	public static final class Node
+		implements ThreadStatNodeTraversal
+	{
+		/** The method being tracked. */
+		protected final MethodTracker.TrackedMethod method;
+		
+		/** Nodes within this tree. */
+		private final Map<MethodTracker.TrackedMethod, Node> _nodes =
+			new LinkedHashMap<>();
+		
+		/**
+		 * Initializes the node for tracking this method.
+		 *
+		 * @param __m The method to track.
+		 * @throws NullPointerException On null arguments.
+		 * @since 2018/02/19
+		 */
+		private Node(MethodTracker.TrackedMethod __m)
+			throws NullPointerException
+		{
+			if (__m == null)
+				throw new NullPointerException();
+			
+			this.method = __m;
+		}
+		
+		/** 
+		 * {@inheritDoc}
+		 * @since 2018/02/19
+		 */
+		@Override
+		public final ThreadStat.Node subNode(MethodTracker.TrackedMethod __m)
+			throws NullPointerException
+		{
+			if (__m == null)
+				throw new NullPointerException();
+		
+			Map<MethodTracker.TrackedMethod, Node> nodes = this._nodes;
+			synchronized (nodes)
+			{
+				Node rv = nodes.get(__m);
+				if (rv == null)
+					nodes.put(__m, (rv = new Node(__m)));
+				return rv;
+			}
+		}
+	
+		/** 
+		 * {@inheritDoc}
+		 * @since 2018/02/19
+		 */
+		@Override
+		public final ThreadStat.Node[] subNodes()
+		{
+			Map<MethodTracker.TrackedMethod, Node> nodes = this._nodes;
+			synchronized (nodes)
+			{
+				Collection<Node> values = nodes.values();
+				return values.<Node>toArray(new Node[values.size()]);
+			}
+		}
 	}
 }
 
