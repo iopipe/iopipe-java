37212fd36c671f7f06a577d23bd0e2105a8ca87e
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-04-13 16:18:52 -0400

    Add base package for the event-info plugin.

---
+++ src/main/java/com/iopipe/plugin/eventinfo/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains the event info plugin which is used as a means to
+ * help determine where an invocation has come from.
+ *
+ * @since 2018/04/13
+ */
+
+package com.iopipe.plugin.eventinfo;
+
