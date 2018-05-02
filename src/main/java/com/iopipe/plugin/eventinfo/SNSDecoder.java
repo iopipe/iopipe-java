ac93d656dc4e0c6c693b2c0aa65d493de9c11fed
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-05-02 14:45:46 -0400

    Handling of SNS and Scheduled events.

--- src/main/java/com/iopipe/plugin/eventinfo/SNSDecoder.java
+++ src/main/java/com/iopipe/plugin/eventinfo/SNSDecoder.java
@@ -1,6 +1,8 @@
 package com.iopipe.plugin.eventinfo;
 
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * This class implements the decoder for SNS events.
@@ -21,7 +23,39 @@ public final class SNSDecoder
 		if (__a == null || __v == null)
 			throw new NullPointerException();
 		
-		throw new Error("TODO");
+		SNSEvent v = (SNSEvent)__v;
+		
+		// Only record the first record
+		List<SNSEvent.SNSRecord> records = v.getRecords();
+		if (records == null || records.size() <= 0)
+			return;
+		SNSEvent.SNSRecord record = records.get(0);
+		
+		__a.accept("Records[0].EventSubscriptionArn",
+			record.getEventSubscriptionArn());
+		
+		SNSEvent.SNS sns = record.getSNS();
+		if (sns != null)
+		{
+			__a.accept("Records[0].Sns.MessageId",
+				sns.getMessageId());
+			__a.accept("Records[0].Sns.Signature",
+				sns.getSignature());
+			__a.accept("Records[0].Sns.SignatureVersion",
+				sns.getSignatureVersion());
+			__a.accept("Records[0].Sns.SigningCertUrl",
+				sns.getSigningCertUrl());
+			__a.accept("Records[0].Sns.UnsubscribeUrl",
+				sns.getUnsubscribeUrl());
+			__a.accept("Records[0].Sns.Subject",
+				sns.getSubject());
+			__a.accept("Records[0].Sns.Timestamp",
+				Objects.toString(sns.getTimestamp(), null));
+			__a.accept("Records[0].Sns.TopicArn",
+				sns.getTopicArn());
+			__a.accept("Records[0].Sns.Type",
+				sns.getType());
+		}
 	}
 	
 	/**
--- src/main/java/com/iopipe/plugin/eventinfo/ScheduledDecoder.java
+++ src/main/java/com/iopipe/plugin/eventinfo/ScheduledDecoder.java
@@ -1,6 +1,8 @@
 package com.iopipe.plugin.eventinfo;
 
 import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * This class implements the decoder for Scheduled events.
@@ -21,7 +23,24 @@ public final class ScheduledDecoder
 		if (__a == null || __v == null)
 			throw new NullPointerException();
 		
-		throw new Error("TODO");
+		ScheduledEvent v = (ScheduledEvent)__v;
+		
+		__a.accept("account",
+			v.getAccount());
+		__a.accept("id",
+			v.getId());
+		__a.accept("region",
+			v.getRegion());
+		
+		List<String> resources = v.getResources();
+		if (resources != null && resources.size() >= 1)
+		{
+			__a.accept("resources[0]",
+				resources.get(0));
+		}
+		
+		__a.accept("time",
+			Objects.toString(v.getTime(), null));
 	}
 	
 	/**
