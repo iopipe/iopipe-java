097fa9a04f78c950ed0a60b32ff626deb8929153
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-07-23 11:33:01 -0400

    Add new classes IOpipeEventUploader and __SerialUploader__.

---
+++ src/main/java/com/iopipe/IOpipeEventUploader.java
@@ -0,0 +1,12 @@
+package com.iopipe;
+
+/**
+ * This interface is implemented by classes which provide an implementation of
+ * events that may be uploaded to the IOpipe service.
+ *
+ * @since 2018/07/23
+ */
+public interface IOpipeEventUploader
+{
+}
+
