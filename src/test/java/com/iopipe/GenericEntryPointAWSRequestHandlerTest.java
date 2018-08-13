818f95e492508c153e2d7cb7e238a80e314c73a3
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-08-13 11:04:02 -0400

    Add blank test to test the generic entry point.

---
+++ src/test/java/com/iopipe/GenericEntryPointAWSRequestHandlerTest.java
@@ -0,0 +1,22 @@
+package com.iopipe;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * This tests the generic AWS request handler.
+ *
+ * @since 2018/08/13
+ */
+public class GenericEntryPointAWSRequestHandlerTest
+{
+	/**
+	 * Tests the generic entry point.
+	 *
+	 * @since 2018/08/13
+	 */
+	@Test
+	public void test()
+	{
+	}
+}
+
