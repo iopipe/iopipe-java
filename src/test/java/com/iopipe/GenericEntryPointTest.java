0060e0ed81cef06c84463a011c6158891c8fd83d
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-08-13 17:53:26 -0400

    Setup entry points for the test and call them for each of the methods in the elsewhere package.

--- src/test/java/com/iopipe/GenericEntryPointTest.java
+++ src/test/java/com/iopipe/GenericEntryPointTest.java
@@ -13,6 +13,27 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class GenericEntryPointTest
 {
+	/** The methods to be tested for entry points. */
+	private static String[] _METHODS =
+		{
+			"com.iopipe.elsewhere.PackagePrivateClass::instancePrivate",
+			"com.iopipe.elsewhere.PackagePrivateClass::instancePackagePrivate",
+			"com.iopipe.elsewhere.PackagePrivateClass::instanceProtected",
+			"com.iopipe.elsewhere.PackagePrivateClass::instancePublic",
+			"com.iopipe.elsewhere.PackagePrivateClass::staticPrivate",
+			"com.iopipe.elsewhere.PackagePrivateClass::staticPackagePrivate",
+			"com.iopipe.elsewhere.PackagePrivateClass::staticProtected",
+			"com.iopipe.elsewhere.PackagePrivateClass::staticPublic",
+			"com.iopipe.elsewhere.PublicClass::instancePrivate",
+			"com.iopipe.elsewhere.PublicClass::instancePackagePrivate",
+			"com.iopipe.elsewhere.PublicClass::instanceProtected",
+			"com.iopipe.elsewhere.PublicClass::instancePublic",
+			"com.iopipe.elsewhere.PublicClass::staticPrivate",
+			"com.iopipe.elsewhere.PublicClass::staticPackagePrivate",
+			"com.iopipe.elsewhere.PublicClass::staticProtected",
+			"com.iopipe.elsewhere.PublicClass::staticPublic",
+		};
+	
 	/**
 	 * Tests the generic entry point.
 	 *
@@ -21,6 +42,23 @@ public class GenericEntryPointTest
 	@Test
 	public void test()
 	{
+		for (String method : _METHODS)
+			try
+			{
+				EntryPoint ep = new EntryPoint(method);
+				
+				if ("squirrel".equals(ep.handle().invokeExact("SQUIRREL")))
+					assertTrue(true, method);
+				else
+					assertTrue(false, method);
+			}
+			catch (Throwable t)
+			{
+				t.printStackTrace();
+				
+				// Failed
+				assertTrue(false, method);
+			}
 	}
 }
 
