1c1737525e406f29a919a67a4d1f374ee2bf5a57
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-08-15 12:33:53 -0400

    Add base for test for the request stream handler.

---
+++ src/test/java/com/iopipe/GenericEntryPointAWSRequestStreamHandlerTest.java
@@ -0,0 +1,26 @@
+package com.iopipe;
+
+import com.iopipe.generic.GenericAWSRequestStreamHandler;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This tests the generic AWS request stream handler.
+ *
+ * @since 2018/08/14
+ */
+public class GenericEntryPointAWSRequestStreamHandlerTest
+{
+	/**
+	 * Tests the generic entry point.
+	 *
+	 * @since 2018/08/14
+	 */
+	@Test
+	public void test()
+		throws Throwable
+	{
+	}
+}
+
