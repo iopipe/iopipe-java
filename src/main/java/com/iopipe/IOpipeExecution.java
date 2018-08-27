9d6b995433075a3f182df47150cfb0b344ab9c1e
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-08-27 14:25:58 -0400

    Change type used.

--- src/main/java/com/iopipe/IOpipeExecution.java
+++ src/main/java/com/iopipe/IOpipeExecution.java
@@ -653,7 +653,7 @@ public final class IOpipeExecution
 			{
 				// If this was a wrapped IOException then instead of reporting our
 				// wrapper instead report the wrapped exception
-				if (thrown instanceof IOpipeWrappedException)
+				if (thrown instanceof SimpleRequestStreamHandlerWrapper.__IOException__)
 				{
 					Throwable instead = thrown.getCause();
 					if (instead != null)
