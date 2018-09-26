3d0852d700018b38df84dfa03707f866ef54e6ab
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-09-26 13:13:56 -0400

    Add a bunch of methods which forward to the logger.

--- src/main/java/com/iopipe/plugin/logger/LoggerExecution.java
+++ src/main/java/com/iopipe/plugin/logger/LoggerExecution.java
@@ -9,9 +9,10 @@ import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
+import java.nio.CharBuffer;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 import javax.json.JsonObject;
 import org.pmw.tinylog.Logger;
@@ -64,6 +65,194 @@ public final class LoggerExecution
 		{
 			throw new RuntimeException("Could not initialize the logger.", e);
 		}
+	}
+	
+	/**
+	 * Logs the given message.
+	 *
+	 * @param __v The logging level.
+	 * @param __c The message used.
+	 * @since 2018/09/26
+	 */
+	public final void log(Enum<?> __v, char[] __c)
+	{
+		this.log(System.currentTimeMillis(), (__v != null ? __v.name() : null),
+			(__c != null ? CharBuffer.wrap(__c) : null));
+	}
+	
+	/**
+	 * Logs the given message.
+	 *
+	 * @param __utcms The current time in UTC milliseconds.
+	 * @param __v The logging level.
+	 * @param __c The message used.
+	 * @since 2018/09/26
+	 */
+	public final void log(long __utcms, Enum<?> __v, char[] __c)
+	{
+		this.log(__utcms, (__v != null ? __v.name() : null),
+			(__c != null ? CharBuffer.wrap(__c) : null));
+	}
+	
+	/**
+	 * Logs the given message.
+	 *
+	 * @param __v The logging level.
+	 * @param __c The message used.
+	 * @param __o The offset.
+	 * @param __l The length.
+	 * @throws IndexOutOfBoundsException If the offset and/or length are
+	 * negative or exceed the array bounds.
+	 * @since 2018/09/26
+	 */
+	public final void log(Enum<?> __v, char[] __c, int __o, int __l)
+		throws IndexOutOfBoundsException
+	{
+		this.log(System.currentTimeMillis(), (__v != null ? __v.name() : null),
+			(__c != null ? CharBuffer.wrap(__c, __o, __l) : null));
+	}
+	
+	/**
+	 * Logs the given message.
+	 *
+	 * @param __utcms The current time in UTC milliseconds.
+	 * @param __v The logging level.
+	 * @param __c The message used.
+	 * @param __o The offset.
+	 * @param __l The length.
+	 * @throws IndexOutOfBoundsException If the offset and/or length are
+	 * negative or exceed the array bounds.
+	 * @since 2018/09/26
+	 */
+	public final void log(long __utcms, Enum<?> __v, char[] __c, int __o,
+		int __l)
+		throws IndexOutOfBoundsException
+	{
+		this.log(__utcms, (__v != null ? __v.name() : null),
+			(__c != null ? CharBuffer.wrap(__c, __o, __l) : null));
+	}
+	
+	/**
+	 * Logs the given message.
+	 *
+	 * @param __v The logging level.
+	 * @param __msg The message used.
+	 * @since 2018/09/26
+	 */
+	public final void log(Enum<?> __v, CharSequence __msg)
+	{
+		this.log(System.currentTimeMillis(), (__v != null ? __v.name() : null), __msg);
+	}
+	
+	/**
+	 * Logs the given message.
+	 *
+	 * @param __utcms The current time in UTC milliseconds.
+	 * @param __v The logging level.
+	 * @param __msg The message used.
+	 * @since 2018/09/26
+	 */
+	public final void log(long __utcms, Enum<?> __v, CharSequence __msg)
+	{
+		this.log(__utcms, (__v != null ? __v.name() : null), __msg);
+	}
+	
+	/**
+	 * Logs the given message.
+	 *
+	 * @param __v The logging level.
+	 * @param __c The message used.
+	 * @since 2018/09/26
+	 */
+	public final void log(String __v, char[] __c)
+	{
+		this.log(System.currentTimeMillis(), __v,
+			(__c != null ? CharBuffer.wrap(__c) : null));
+	}
+	
+	/**
+	 * Logs the given message.
+	 *
+	 * @param __utcms The current time in UTC milliseconds.
+	 * @param __v The logging level.
+	 * @param __c The message used.
+	 * @since 2018/09/26
+	 */
+	public final void log(long __utcms, String __v, char[] __c)
+	{
+		this.log(__utcms, __v,
+			(__c != null ? CharBuffer.wrap(__c) : null));
+	}
+	
+	/**
+	 * Logs the given message.
+	 *
+	 * @param __v The logging level.
+	 * @param __c The message used.
+	 * @param __o The offset.
+	 * @param __l The length.
+	 * @throws IndexOutOfBoundsException If the offset and/or length are
+	 * negative or exceed the array bounds.
+	 * @since 2018/09/26
+	 */
+	public final void log(String __v, char[] __c, int __o, int __l)
+		throws IndexOutOfBoundsException
+	{
+		this.log(System.currentTimeMillis(), __v,
+			(__c != null ? CharBuffer.wrap(__c, __o, __l) : null));
+	}
+	
+	/**
+	 * Logs the given message.
+	 *
+	 * @param __utcms The current time in UTC milliseconds.
+	 * @param __v The logging level.
+	 * @param __c The message used.
+	 * @param __o The offset.
+	 * @param __l The length.
+	 * @throws IndexOutOfBoundsException If the offset and/or length are
+	 * negative or exceed the array bounds.
+	 * @since 2018/09/26
+	 */
+	public final void log(long __utcms, String __v, char[] __c, int __o,
+		int __l)
+		throws IndexOutOfBoundsException
+	{
+		this.log(__utcms, __v,
+			(__c != null ? CharBuffer.wrap(__c, __o, __l) : null));
+	}
+	
+	/**
+	 * Logs the given message.
+	 *
+	 * @param __v The logging level.
+	 * @param __msg The message used.
+	 * @since 2018/09/26
+	 */
+	public final void log(String __v, CharSequence __msg)
+	{
+		this.log(System.currentTimeMillis(), __v, __msg);
+	}
+	
+	/**
+	 * Logs the given message.
+	 *
+	 * @param __utcms The current time in UTC milliseconds.
+	 * @param __v The logging level.
+	 * @param __msg The message used.
+	 * @since 2018/09/26
+	 */
+	public final void log(long __utcms, String __v, CharSequence __msg)
+	{
+		// Use a default level
+		if (__v == null)
+			__v = "NONE";
+		
+		// Use a default message
+		if (__msg == null)
+			__msg = "No message.";
+		
+		throw new Error("TODO");
 	}
 	
 	/**
