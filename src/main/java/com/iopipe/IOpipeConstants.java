0c414c95a1159858dcf50853057f255108336933
Via CircleCI <dev@iopipe.com>
2018-10-09 15:05:37 +0000

    [RELEASE 1.9.0] Update IOpipeConstants and README.md

--- src/main/java/com/iopipe/IOpipeConstants.java
+++ src/main/java/com/iopipe/IOpipeConstants.java
@@ -15,7 +15,7 @@ public interface IOpipeConstants
 {
 	/** The version for this agent. */
 	public static final String AGENT_VERSION =
-		"1.8.0";
+		"1.9.0";
 	
 	/** This is used to determine the load time of the service. */
 	public static final long LOAD_TIME =
