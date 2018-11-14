7e85ac0490621af5defdcc60bbc54fb66abab59f
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-11-14 13:51:25 -0500

    Add base classes for lighter service connections.

---
+++ src/main/java/com/iopipe/http/LighterServiceConnectionFactory.java
@@ -0,0 +1,26 @@
+package com.iopipe.http;
+
+/**
+ * This is a factory which can create connections to the remote IOpipe service
+ * to send reports.
+ *
+ * @since 2018/11/14
+ */
+public final class LighterServiceConnectionFactory
+	implements RemoteConnectionFactory
+{
+	/**
+	 * {@inheritDoc}
+	 * @since 2018/11/14
+	 */
+	@Override
+	public RemoteConnection connect(String __url, String __auth)
+		throws NullPointerException, RemoteException
+	{
+		if (__url == null)
+			throw new NullPointerException();
+		
+		throw new Error("TODO");
+	}
+}
+
