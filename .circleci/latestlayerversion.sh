c00e96353d07c8e17670604fbbba75a79c3a1b03
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-12-20 12:09:36 -0500

    Pull in latest layer version from the Python agent.

---
+++ .circleci/latestlayerversion.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+REGIONS=(
+  ap-northeast-1
+  ap-northeast-2
+  ap-south-1
+  ap-southeast-1
+  ap-southeast-2
+  ca-central-1
+  eu-central-1
+  eu-west-1
+  eu-west-2
+  eu-west-3
+  #sa-east-1
+  us-east-1
+  us-east-2
+  us-west-1
+  us-west-2
+)
+
+echo ""
+echo "java8:"
+echo ""
+
+for region in "${REGIONS[@]}"; do
+    latest_arn=$(aws --region $region lambda list-layer-versions --layer-name IOpipeJava8 --output text --query "LayerVersions[0].LayerVersionArn")
+    echo "* ${region}: \`${latest_arn}\`"
+done
