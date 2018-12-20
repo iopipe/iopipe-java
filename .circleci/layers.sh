3f6c08b768aca8a6154a09e960234e8101b1356c
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-12-20 15:25:57 -0500

    ZIP everything up.

--- .circleci/layers.sh
+++ .circleci/layers.sh
@@ -5,5 +5,13 @@ mvn clean
 mvn package -Dmaven.test.skip=true
 mvn dependency:copy-dependencies -DincludeScope=runtime -DoutputDirectory=target
 
+# Copy target JARs to library directory
+rm -rvf java
+mkdir -p java/lib/
+cp -v target/*.jar java/lib/
+
+# ZIP it up
+zip -rq java8.zip java
+
 exit 7
 
