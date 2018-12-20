66cd83bef66c77056df45f9372c850e283fd9c72
Stephanie Gawroriski <xer@multiphasicapps.net>
2018-12-20 12:59:10 -0500

    Build fresh JAR and place in all the dependencies.

--- .circleci/layers.sh
+++ .circleci/layers.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/sh -e
+
+# Package JAR and place all dependencies into the target
+mvn clean
+mvn package -Dmaven.test.skip=true
+mvn dependency:copy-dependencies -DincludeScope=runtime -DoutputDirectory=target
 
 exit 7
 
